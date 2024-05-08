bd59f8d1363b8e659cea5647d95eedd9f9036c2b
aD3nnis <avadennis117@gmail.com>
2024-05-07 19:50:13 -0700

    gsap works

---
+++ Typography/typo.js
@@ -0,0 +1,8 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code goes here
+    let gsapBigImage = document.querySelector(".gsapBigImage");
+    gsap.to(gsapBigImage, { x: 200 });
+});
