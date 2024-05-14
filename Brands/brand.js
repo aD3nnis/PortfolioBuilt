18a5363cb62f33480946402f91bdcc152deba3ac
aD3nnis <avadennis117@gmail.com>
2024-05-14 14:32:40 -0700

    flickering logo

--- Brands/brand.js
+++ Brands/brand.js
@@ -6,3 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
     gsap.to(gsapBigImage, { duration: 2, opacity: 1 });
     // gsap.to(gsapBigImage, { duration: 1, scaleX: 1.2, scaleY: 1.2 });
 });
+function flickerLogo() {
+    // Randomly generate the opacity values
+    const opacity1 = Math.random();
+    const opacity2 = Math.random();
+
+    // Duration of the flicker animation
+    const duration = gsap.utils.random(0.2, 0.6); // Random duration between 0.1 and 0.5 seconds
+
+    // Animate the opacity back and forth
+    gsap.to("#overMainLogo", { opacity: opacity1, duration: duration, yoyo: true, repeat: 1, onComplete: flickerLogo });
+    gsap.to("#overMainLogo", { opacity: opacity2, duration: duration, yoyo: true, repeat: 1 });
+  }
+
+  // Start the flickering effect
+  flickerLogo();
