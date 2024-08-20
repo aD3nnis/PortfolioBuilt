077b2b6ee35bbe1dafe7fc49b2c831d72495a0a5
aD3nnis <avadennis117@gmail.com>
2024-08-20 13:04:04 -0700

    changed gsap to being from cdn

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@
 console.log("in it ")
 document.addEventListener("DOMContentLoaded", function() {
     // Your JavaScript code goes here
-    // let idPortfolio = document.querySelector(".portfolioText");
-    // gsap.to(idPortfolio, { duration: 2, opacity: 1 });
+    let idPortfolio = document.querySelector(".portfolioText");
+    gsap.to(idPortfolio, { duration: 2, opacity: 1 });
 });
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
