e2da12b24811a246d43590e8b343cab587cba530
aD3nnis <avadennis117@gmail.com>
2024-08-19 21:09:09 -0700

    rid of gsap with hero page

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@
 console.log("in it ")
 document.addEventListener("DOMContentLoaded", function() {
     // Your JavaScript code goes here
-    let idPortfolio = document.querySelector(".portfolioText");
-    gsap.to(idPortfolio, { duration: 2, opacity: 1 });
-    // gsap.to(gsapBigImage, { duration: 1, scaleX: 1.2, scaleY: 1.2 });
+    // let idPortfolio = document.querySelector(".portfolioText");
+    // gsap.to(idPortfolio, { duration: 2, opacity: 1 });
 });
