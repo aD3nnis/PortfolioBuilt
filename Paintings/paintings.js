85277243599a5c8fa0c71a0d115eda7831061393
aD3nnis <avadennis117@gmail.com>
2024-08-22 17:08:13 -0700

    added imgs

---
+++ Paintings/paintings.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+let sections = gsap.utils.toArray(".slide");
+const totalWidth = sections.reduce((acc, section) => {
+    return acc + section.getBoundingClientRect().width;
+  }, 0);
+    
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#iframeId",
+    pin: ".pinned-elements",
+    start: 'top top',
+    pinSpacing: false,
+    scrub: 1,
+    end: `+=${totalWidth} bottom` ,
+    markers: {
+        startColor: "green",
+        endColor: "red",
+        fontSize: "12px",
+        fontWeight: "bold",
+    },
+    onUpdate: (self) => {
+        // Remove any unexpected top adjustments
+        if (self.pin) {
+          self.pin.style.top = "0px";
+        }
+      }
+  }
+});
