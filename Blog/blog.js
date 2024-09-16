5d76fab3624b3b917f73b4b80d456d50a98f5c51
aD3nnis <avadennis117@gmail.com>
2024-09-16 13:45:35 -0700

    animations good

--- Blog/blog.js
+++ Blog/blog.js
@@ -18,3 +18,21 @@
                 }
             }
         );
+        gsap.fromTo(".experience-text", 
+            {
+                x: -100,
+                opacity: 0
+            }, 
+            {
+                x: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: ".halo-text",
+                    start: "top-=100px top",
+                    endTrigger: ".halo-experience-img",
+                    end: "top top",
+                    scrub: true, // Adjust animation based on scroll position
+                    // markers: true // Remove this line in production
+                }
+            }
+        );
