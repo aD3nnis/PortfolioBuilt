c0260f7770511a1d34fb8a0092619b579cba59ce
aD3nnis <avadennis117@gmail.com>
2024-09-16 22:19:53 -0700

    music marty

--- Blog/blog.js
+++ Blog/blog.js
@@ -152,6 +152,25 @@
                 }
             }
         );
+        gsap.fromTo(".music-text", 
+            {
+                y: 200,
+                opacity: 0
+            }, 
+            {
+                y: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: ".halo-journey-img",
+                    start: "top top",
+                    endTrigger: ".halo-atmosphere-img",
+                    end: "bottom top",
+                    scrub: true, // Adjust animation based on scroll position
+                    // markers: true // Remove this line in production
+                }
+            }
+            
+        );
           
           
           
