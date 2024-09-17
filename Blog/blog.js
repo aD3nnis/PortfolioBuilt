9af687ae58643551592681e7b8431a7e1f13f271
aD3nnis <avadennis117@gmail.com>
2024-09-16 18:47:05 -0700

    best

--- Blog/blog.js
+++ Blog/blog.js
@@ -39,18 +39,47 @@
         );
 
 
-        gsap.fromTo(".testing-container", 
-            {
+        // gsap.fromTo("textPath", 
+        //     {
+        //         opacity: 0, // Start fully outside the view
+        //     },
+        //     {
+        //         opacity: 1,  // End fully inside the view
+        //         scrollTrigger: {
+        //         trigger: ".halo-experience-img",
+        //         start: "top top", // Start animation when .halo-experience-img reaches the top of the viewport
+        //         endTrigger: ".halo-atmosphere-img",
+        //         end: "top top", // End animation when .halo-atmosphere-img reaches the top of the center of the viewport
+        //         scrub: true, // Smoothly syncs the animation progress with the scrollbar
+        //     }
+        //     }
+        //   );
+          // GSAP animation setup
+          gsap.utils.toArray('.letter').forEach((letter, index) => {
+            // Calculate stagger delay based on index
+            const staggerDelay = (index/2) * 0.16; 
+          
+            gsap.fromTo(letter,
+                            {
                 opacity: 0, // Start fully outside the view
             },
-            {
-                opacity: 1,  // End fully inside the view
-                scrollTrigger: {
-                trigger: ".halo-experience-img",
-                start: "top top", // Start animation when .halo-experience-img reaches the top of the viewport
-                endTrigger: ".halo-atmosphere-img",
-                end: "top top", // End animation when .halo-atmosphere-img reaches the top of the center of the viewport
-                scrub: true, // Smoothly syncs the animation progress with the scrollbar
-            }
-            }
-          );
+                 {
+            pacity:1,
+              scrollTrigger: {
+                trigger: '.halo-experience-img',
+                start: 'center top',
+                endTrigger: ".halo-journey-img",
+                end: "center top",// Adjust the end point based on when you want the animation to end
+                scrub: true, // Link animation progress to scroll position
+                onUpdate: self => {
+                  // Calculate opacity based on scroll progress
+                  const scrollProgress = (self.progress - staggerDelay) / (1 - staggerDelay);
+                  const clampedProgress = Math.min(1, Math.max(0, scrollProgress));
+                  gsap.set(letter, { opacity: clampedProgress });     },
+              },
+            });
+          });
+          
+          
+          
+          
