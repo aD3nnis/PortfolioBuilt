406481ed577556f8e200339ba0580f4370f60726
aD3nnis <avadennis117@gmail.com>
2024-09-16 19:16:08 -0700

    more

--- Blog/blog.js
+++ Blog/blog.js
@@ -1,5 +1,38 @@
         gsap.registerPlugin(ScrollTrigger);
-
+        gsap.fromTo(".experience-text", 
+            {
+                x: -100,
+                opacity: 0
+            }, 
+            {
+                x: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: ".halo-text",
+                    start: "top-=200px top",
+                    endTrigger: ".halo-experience-img",
+                    end: "top top",
+                    scrub: true, // Adjust animation based on scroll position
+                    // markers: true // Remove this line in production
+                }
+            }
+        );
+        gsap.fromTo(".halo-text", 
+            {
+                opacity: 0
+            }, 
+            {
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: ".halo-text",
+                    start: "top-=600px top",
+                    endTrigger: ".halo-text",
+                    end: "bottom top",
+                    scrub: true, // Adjust animation based on scroll position
+                    // markers: true // Remove this line in production
+                }
+            }
+        );
         gsap.fromTo(".journey-text", 
             {
                 y: 200,
@@ -19,41 +52,8 @@
             }
             
         );
-        gsap.fromTo(".experience-text", 
-            {
-                x: -100,
-                opacity: 0
-            }, 
-            {
-                x: 0,
-                opacity: 1,
-                scrollTrigger: {
-                    trigger: ".halo-text",
-                    start: "top-=100px top",
-                    endTrigger: ".halo-experience-img",
-                    end: "top top",
-                    scrub: true, // Adjust animation based on scroll position
-                    // markers: true // Remove this line in production
-                }
-            }
-        );
 
 
-        // gsap.fromTo("textPath", 
-        //     {
-        //         opacity: 0, // Start fully outside the view
-        //     },
-        //     {
-        //         opacity: 1,  // End fully inside the view
-        //         scrollTrigger: {
-        //         trigger: ".halo-experience-img",
-        //         start: "top top", // Start animation when .halo-experience-img reaches the top of the viewport
-        //         endTrigger: ".halo-atmosphere-img",
-        //         end: "top top", // End animation when .halo-atmosphere-img reaches the top of the center of the viewport
-        //         scrub: true, // Smoothly syncs the animation progress with the scrollbar
-        //     }
-        //     }
-        //   );
           // GSAP animation setup
           gsap.utils.toArray('.letter').forEach((letter, index) => {
             // Calculate stagger delay based on index
